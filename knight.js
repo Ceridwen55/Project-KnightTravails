15b48610a9c473aa97dca32c62b28789358297f9
Zaqzulkan <zulkanzaqzuq@gmail.com>
2024-11-03 13:17:27 +0700

    Almost done and just to be saved

--- knight.js
+++ knight.js
@@ -15,7 +15,30 @@ const knightMoves = (start,end) =>
     const beenThere = new Set();
     beenThere.add(start.toString());
 
+    // Loop the queue to process each position
+    while (queue.length > 0)
+    {
+        const [current,path] = queue.shift();
+        const [x,y] = current;
 
+        if (x === end[0] && y === end[1])
+        {
+            console.log(`You made it in ${path.length-1} moves, here is the breakdown :`)
+            path.forEach((step)) => console.log(step);
+            return path
+        }
+
+        //EXplore and put limits based on 8 x 8 board limitation
+
+        for (const [dx,dy] of moves )
+        {
+            const [newX,newY] = [x + dx, y + dy];
+            if (newX >= 0 && newX < 8 && newY >=0 && newY < 8)
+            {
+
+            }
+        }
+    }
 
 
 };
